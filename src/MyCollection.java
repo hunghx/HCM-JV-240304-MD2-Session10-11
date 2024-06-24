1a043117aa0cf157d68be315d65dd5dde7c857d7
hunghx <baby10051999@gmail.com>
2024-06-24 12:05:43 +0700

    up

--- src/MyCollection.java
+++ src/MyCollection.java
@@ -147,5 +147,82 @@ public class MyCollection {
         stack.peek(); // 10 ko xóa
         int index = stack.search(5);
         System.out.println(index);
+
+        Queue<String> queueString = new LinkedList<>();
+        // thêm mới phần tuwr vào hàng đợi : enqueue
+        queueString.offer("hung 1");
+        queueString.offer("hung 2");
+        queueString.offer("hung 3");
+        queueString.offer("hung 4");
+        //lấy phân tu ra khoi hàng đơi : dequeue
+//        System.out.println("Phần tử đầu hàng đợi là : "+ queueString.poll());
+        while (!queueString.isEmpty()){
+            String element = queueString.poll();
+            System.out.println("Phần tử đang đc xử lí là :"+element);
+        }
+
+        // hoat động thêm mới phan tu trong stack gọi là PUSH ;
+        // hoat động lấy ra phan tu trong stack gọi là POP ;
+        // LIFO
+        Stack<Integer> stack1 = new Stack<>();
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
+        while (!stack1.isEmpty()){
+            Integer element = stack1.pop();
+            System.out.println(element);
+        }
+
+        // dùng stack để kiểm tra dấu ngoặc của biểu thức
+        // ((2*3)+5*(10-4))(
+
+        // B1 , tạo 1 stack lưu trữ các dấu ngoặc mở
+        // B2 duyệt laanf lượt các kí tự trong bi thức
+        // B3 nếu gặp dấu ngoặc mở thì thêm vào stack
+        // nếu gặp dấu ngoặc đóng thì kiê tra xem có khop với phần t trên cùng của stack hay ko ;
+        // nếu có thì loai phần tử trên cùng, nếu ko thì biểu thức sai
+        // tieps tục thc hiện lại B2 đến khi hết biểu thức .
+        // B5 nếu duyệt hết mà stack rỗng thì biểu thức dúng , nguoc lại thì là sai
+
+        Stack<String> stackChar = new Stack<>();
+        boolean check = true;
+        // chuỗi nhập vào
+        String input = "((2*3)+[5*(]10-4))";
+        for (String c : input.split("")){ // chuyeenr chuoi thanh mảng
+            if (c.equals("{")||c.equals("[")||c.equals("(")){
+                // them vao stack
+                stackChar.push(c);
+            }else if (c.equals("}")||c.equals("]")||c.equals(")")){
+                // kiểm tra tính hợp lê
+                // lấy phan tu ra khoi stack
+                String in = stackChar.peek();
+                // kiểm tra
+                if (isValid(in,c)){
+                    // đúng
+                    stackChar.pop();
+                }else {
+                    check = false;
+                    break;
+                }
+            }
+        }
+        // kiểm tra xem stack có rỗng không
+        if (!stackChar.isEmpty()){
+            check = false;
+        }
+
+        if (check){
+            System.out.println("Bieu thu hop le");
+        }else {
+            System.err.println("Bieu thu ko hop lẹ");
+        }
+
+    }
+    private  static boolean isValid(String in, String out){
+        boolean con1 = in.equals("(") && out.equals(")");
+        boolean con2 = in.equals("{") && out.equals("}");
+        boolean con3 = in.equals("[") && out.equals("]");
+        return  con1||con2||con3 ;
     }
 }
